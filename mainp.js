0ffe15ba9dd00edbae2db6883101fbed812cffd5
birajlamichhane75 <birajlamichhane57@gmail.com>
2024-03-05 23:02:08 +0545

    project added

--- mainp.js
+++ mainp.js
@@ -6,7 +6,7 @@ let projectArr = [
     
     {
         video:"videos/techinfo.mp4",
-        tools:["Next JS","Mongodb"],
+        tools:["Next JS","Mongodb","TailWind CSS"],
         title:"TechInfo",
         url:"https://biraj-techinfo.vercel.app",
     },
@@ -24,7 +24,7 @@ let projectArr = [
     },
     {
         video:"videos/todolist.mp4",
-        tools:["Next JS","Mongodb"],
+        tools:["Next JS","Mongodb","TailWind CSS"],
         title:"Fullstack To-Do-List",
         url:"https://github.com/birajlamichhane75/Todo-list-Next-js",
     },
